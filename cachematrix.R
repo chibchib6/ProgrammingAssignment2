4eb07b2026a57d44d92c209991ce86cb72c9b8c6
chib <chib@cisco.com>
2019-03-30 00:11:38 -0700

    comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,10 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# This R file contains 2 functions: makeCacheMatrix and cacheSolve
+## Collectively, the functions cache and compute the inverse of a matrix
+
+## makeCacheMatrix() returns a special matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
@@ -20,6 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+## cacheSolve() solves for the inverse of the special matrix created via makeCacheMatrix()
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
